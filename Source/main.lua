d4454847f7c392995f4783f0f8c7c3f7b02bce36
Adam Chamberlin <adam@mackman.co.uk>
2024-05-30 16:07:34 +0100

    Update main.lua

--- Source/main.lua
+++ Source/main.lua
@@ -2,12 +2,17 @@
 -- (use the `import "myFilename"` command), but the simplest games can be written
 -- with just `main.lua`.
 
--- You'll want to import these in just about every project you'll work on.
-
+-- Common CoreLibs imports.
 import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
+
+-- Project imports
+import "button"
+import "crank"
+import "lifecycle"
+import "simulator"
 
 -- Declaring this "gfx" shorthand will make your life easier. Instead of having
 -- to preface all graphics calls with "playdate.graphics", just use "gfx."
@@ -15,9 +20,32 @@ import "CoreLibs/timer"
 -- NOTE: Because it's local, you'll have to do it in every .lua source file.
 local gfx = playdate.graphics
 
-gfx.setColor(gfx.kColorBlack)
+--- By convention, most games need to perform some initial setup when they're
+--- initially launched. Perform that setup here.
+---
+--- Note: This will be called exactly once. If you're looking to do something
+--- whenever the game is resumed from the background, see playdate.gameWillResume
+--- in lifecycle.lua
+local function gameDidLaunch()
+    print(playdate.metadata.name .. " launched!")
 
+    gfx.setBackgroundColor(gfx.kColorBlack)
+end
+gameDidLaunch()
+
+--- This update method is called once per frame.
 function playdate.update()
+    -- Example code. Draw a full-screen rectangle and the frames per second
     gfx.fillRect(0, 0, 400, 240)
     playdate.drawFPS(0,0)
+
+    -- Update and draw all sprites. Calling this method in playdate.update
+    -- is generally what you want, if you're using sprites.
+    -- See https://sdk.play.date/1.9.3/#f-graphics.sprite.update for more info
+    gfx.sprite.update()
+
+    -- Update all timers once per frame. This is required if you're using
+    -- timers in your game.
+    -- See https://sdk.play.date/1.9.3/#f-timer.updateTimers for more info
+    playdate.timer.updateTimers()
 end
